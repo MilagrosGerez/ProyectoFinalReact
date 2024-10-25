24d6fdafe4f44867ca5705e1ee8263c6f7e3ec69
Milagros Gerez <gerezmilagros03@gmail.com>
2024-10-25 17:41:12 -0300

    subiendo proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
-import NavBar from './components/navBar';
+import NavBar from './components/NavBar';
 import CartWidget from './components/CartWidget';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailcontainer';
